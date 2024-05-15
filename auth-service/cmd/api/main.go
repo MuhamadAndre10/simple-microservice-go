fab7f13ec9dc9d537f94658ad37bf652d75803b2
MuhamadAndre10 <andrepriyanto95@gmail.com>
2024-05-15 10:49:09 +0700

    add: routes for auth service

--- auth-service/cmd/api/main.go
+++ auth-service/cmd/api/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"github.com/MuhamadAndre10/simple-microservice/auth-service/data"
+	"log"
+	"net/http"
+)
 
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	// TODO connect to DB
+
+	// set up config
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- auth-service/go.mod
+++ auth-service/go.mod
@@ -1,3 +1,7 @@
 module github.com/MuhamadAndre10/simple-microservice/auth-service
 
 go 1.22.1
+
+require golang.org/x/crypto v0.23.0
+
+require github.com/go-chi/cors v1.2.1 // indirect
